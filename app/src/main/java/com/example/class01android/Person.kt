056ec910f83afe9b62e898f20a8e96e532128d03
Dawn Nguyen <lophocpzo@gmail.com>
2021-09-25 20:37:06 +0700

    refactor: Refactor Class Person.kt so that my app can work every year instead 2021

--- app/src/main/java/com/example/class01android/Person.kt
+++ app/src/main/java/com/example/class01android/Person.kt
@@ -1,12 +1,15 @@
 package com.example.class01android
 
+import java.util.*
+
 class Person(var firstName: String, var lastName: String, var birthYear: Int) {
+    val cal: Calendar = Calendar.getInstance()
     val fullName: String
         get() {
             return "$firstName $lastName"
         }
     val age: Int
         get() {
-            return 2021 - birthYear
+            return cal.get(Calendar.YEAR) - birthYear
         }
 }
